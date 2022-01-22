21abfae34732a6b94e03952876fd51539a92ec3c
shirondru <59421145+shirondru@users.noreply.github.com>
2022-01-22 14:40:50 -0800

    Add more thorough comments

--- mst/graph.py
+++ mst/graph.py
@@ -22,22 +22,29 @@ class Graph:
             return np.loadtxt(f, delimiter=',')
 
     def construct_mst(self,starting_node=0):
-        """ Given `self.adj_mat`, the adjacency matrix of a connected undirected graph, implement Prim's 
-        algorithm to construct an adjacency matrix encoding the minimum spanning tree of `self.adj_mat`. 
-            
-        `self.adj_mat` is a 2D numpy array of floats. 
-        Note that because we assume our input graph is undirected, `self.adj_mat` is symmetric. 
-        Row i and column j represents the edge weight between vertex i and vertex j. An edge weight of zero indicates that no edge exists. 
-        
-        TODO: 
-            This function does not return anything. Instead, store the adjacency matrix 
-        representation of the minimum spanning tree of `self.adj_mat` in `self.mst`.
-        We highly encourage the use of priority queues in your implementation. See the heapq
-        module, particularly the `heapify`, `heappop`, and `heappush` functions.
-        
+        """ 
+        starting_node: adj_mat index for node that will be used to start MST construction from. This is included in the method
+        to facilitate a test, `test_find_unique_mst` in `test_mst.py` that a graph with unique edge weights will have a unique MST by constructing an MST from all possible start nodes 
+        and checking the same MST is constructed
 
-        starting_node: index of adj_mat for node that will be used to start MST construction from. This is included in the method
-        to facilitate a test that a graph with unique edge weights will have a unique MST by constructing an MST from all possible start nodes
+        What construct_mst is doing:
+        1) Instantiate the visited_vertices list with an arbitrarily chosen starting node (default is node from idx 0 of adj_mat but can be chosen as an argument)
+        2) instantiate an empty heap for the priority queue
+        3) Instantiate an empty 2D nunpy array of the same shape as adj_mat to hold the MST
+        4) Add outgoing edges from this starting node to the heap (priority queue), so that you can pick the lowest weight edge to grow the tree.
+            4a) look for outgoing edges using `add_edges_to_pq`; docstring of this function explains how it works
+        5) While the number of number of nodes within the growing MST (the number of nodes in visited_vertices) is less than the total number of nodes:
+            a)Pop the highest priority edge from the priority queue. This edge will (almost) always be the the lowest weight edge because the priority queue is a heap that prioritizes low weights
+            b)A popped edge is of the form (weight, start_node, destination node). If the destination node has not already been added to the growing MST (i.e, if it is not already in visited_vertices):
+                i) Add the edge to the MST adjacency matrix by adding this edge's weight to the MST at position MST[start_node,destination_node]
+                ii) Also add the edge to the opposite side of the MST diagonal by adding this edge's weight at position MST[destination_node,start_node]. This ensures the MST remains symmetric and therefore an undirected graph
+                iii) At this point, we have grown the MST by adding the minimum weight edge to a vertex not already in the tree. The added edge was the one of minimum weight because a heap was used for the priority queue
+                iv) Add the destination node of the edge to the visited_vertices list
+                v) add all outgoing edges from the node to the priority queue, so edges from this vertex ending at a node not already in the growing MST can possibly be added to the MST (if they are of lowest weight)
+                vi) steps iv and v together ensure no edges starting from a node in the MST end at a node already in the MST and therefore prevents a cycle, which would be impossible in a MST.
+            c) repeat a & b until the MST has the same number of edges as the adjacency matrix (i.e len(vsited_vertices) == num_vertices)
+        6) Save the MST in the self.mst attribute
+
         """
         num_vertices = self.adj_mat.shape[0] #pick one of the 2 dimensions of the symmetric matrix to get the # of vertices
         visited_vertices = [starting_node] #instantiate  list of visited vertices with the 0th vertex arbitrarily chosen to be the starting node
@@ -50,31 +57,31 @@ class Graph:
             pq: priority queue to which outgoing edges will be added
             start_node: All edges from this node will be found and added to the priority queue
 
-            This function adds a tuple to the heapq priority queue of the form (weight, start_node, destination_node)
+            This function adds a tuple to the heap (priority queue) of the form (weight, start_node, destination_node)
 
             Strategy:
             1. Find neighboring nodes of start node that form an edge (weight >0)
-                1a. This is done by indexing the row corresponding to the start node in the adjacency matrix, and looking for indices in that row where the value >0 (those indices are the neighbors that form an edge with source node)
-                1b. Those indices are the neighbors that form an edge
+                1a. This is done by indexing the row corresponding to the start node in the adjacency matrix, and looking for indices in that row where the value > 0 
+                1b. Those elements are the neighbors that form an edge with the source node
             2. Add the tuple containing (weight, start_node index, neighbor index) to the priority queue
 
             '''
 
-            for neighbor in np.argwhere(self.adj_mat[start_node]>0):#find neighboring nodes of start node by finding what other nodes form an edge of weight >0 with the start node.
+            for neighbor in np.argwhere(self.adj_mat[start_node]>0):#find neighboring nodes of start node by finding what other nodes form an edge of weight >0 with the start node. Iterate through those neighbors
 
                 neighbor = neighbor[0]#index of neighbor in adjacency matrix is returned in a list of length 1 after using np.argwhere. Get the actual value by indexing it out.  
                 heapq.heappush(priority_queue,(self.adj_mat[start_node,neighbor],start_node,neighbor)) #add tuple to priority_queue with form (weight,visited_node_idx,neighbor_idx)
 
         add_edges_to_pq(priority_queue,visited_vertices[0])#add edges from the start node in visited_vertices to priority_queue
 
-        while len(visited_vertices) < num_vertices: #while not all vertices have been added to visited_vertices
+        while len(visited_vertices) < num_vertices: #while not all nodes have been added to visited_vertices, and therefore not all nodes have been added to the MST (MST has to be fully connected)
 
-            lowest_weight_edge = heapq.heappop(priority_queue)
-            if lowest_weight_edge[2] not in visited_vertices: #3rd element in tuple representing edge is the destination node. Check if it is already in visited_vertices
-                MST[lowest_weight_edge[1],lowest_weight_edge[2]] = lowest_weight_edge[0] #Add weight (0th idx) of lowest weight edge in PQ to MST as an edge visited vertex (1st idx of tuple) and destination node (2nd idx of tuple)
-                MST[lowest_weight_edge[2],lowest_weight_edge[1]] = lowest_weight_edge[0] #fill in the same edge weight on the other side of the diagonal so the mst adjacency matrix remains undirected
+            lowest_weight_edge = heapq.heappop(priority_queue) #pop lowest weight edge from the priority queue
+            if lowest_weight_edge[2] not in visited_vertices: #3rd element in tuple is the destination node of the edge. Check if it is already in visited_vertices. If it is then there is already an edge connecting a node in the MST to this node, which is already in the MST. That would form a cycle; skip it
+                MST[lowest_weight_edge[1],lowest_weight_edge[2]] = lowest_weight_edge[0] #Add weight (0th idx) of lowest weight edge in PQ to MST. Add weight to position MST[start_node,destination_node]. Start_node = idx 1 of tuple,  destination node = idx 2 of tuple
+                MST[lowest_weight_edge[2],lowest_weight_edge[1]] = lowest_weight_edge[0] #fill in the same edge weight on the other side of the diagonal so the mst adjacency matrix remains symmetric and therefore undirected
                 visited_vertices.append(lowest_weight_edge[2]) #add destination vertex to visited_vertices
-                add_edges_to_pq(priority_queue,lowest_weight_edge[2]) #add edges from destination vertex to priority_queue
+                add_edges_to_pq(priority_queue,lowest_weight_edge[2]) #add outgoing edges from destination vertex to priority_queue
 
-        self.mst = MST
+        self.mst = MST #add finished MST to self.mst attribute
         
--- test/test_mst.py
+++ test/test_mst.py
@@ -10,20 +10,12 @@ def check_mst(adj_mat: np.ndarray,
               mst: np.ndarray, 
               expected_weight: int, 
               allowed_error: float = 0.0001):
-    """ Helper function to check the correctness of the adjacency matrix encoding an MST.
-        Note that because the MST of a graph is not guaranteed to be unique, we cannot 
-        simply check for equality against a known MST of a graph. 
-
+    """ 
         Arguments:
             adj_mat: Adjacency matrix of full graph
             mst: Adjacency matrix of proposed minimum spanning tree
             expected_weight: weight of the minimum spanning tree of the full graph
             allowed_error: Allowed difference between proposed MST weight and `expected_weight`
-
-        TODO: 
-            Add additional assertions to ensure the correctness of your MST implementation
-        For example, how many edges should a minimum spanning tree have? Are minimum spanning trees
-        always connected? What else can you think of?
 
 
 
@@ -33,7 +25,7 @@ def check_mst(adj_mat: np.ndarray,
         has the expected edge weights, as the lower triangle was already validated by `check_mst` and this ensures the two triangles are identical.
         This test checks for symmetry by checking that each element in the MST adjacency matrix and the same element in the transposed matrix are similar within a very small tolerance
         
-        2) Check all edges in MST are also in adj_mat
+        2) Check all edges in MST are also in adj_mat (i.e, there are no new edges)
         The MST of adj_mat should not contain any edges that were not in adj_mat. This test tests that is the case by iterating through
         each element in the lower triangle of the MST. If an edge exists (the element has weight > 0) between those nodes, check the edge of the same weight
         also exists between the same nodes in adj_mat. Because we have also tested for symmetry, only checking the lower triangle is sufficient.
@@ -60,15 +52,15 @@ def check_mst(adj_mat: np.ndarray,
 
 
     #1) Check proposed MST matrix is symmetric
-    assert np.allclose(mst, mst.T), 'Proposed MST adjacency matrix is not symmetric'
+    assert np.allclose(mst, mst.T), 'Proposed MST adjacency matrix is not symmetric' #Check each element in MST and its transpose are identical within a very small threshold to test for symmetry
 
 
 
     #2) Check that all of the edges in MST are also in the adj_mat
     for i in range(mst.shape[0]):
-        for j in range(i+1):
-            if mst[i,j] > 0:
-                assert approx_equal(mst[i,j], adj_mat[i,j]), 'Proposed MST contains an edge not found in the original graph'
+        for j in range(i+1): #iterate through elements in lower triangle of MST
+            if mst[i,j] > 0: #if an element in the lower triangle has weight > 0 it is an edge
+                assert approx_equal(mst[i,j], adj_mat[i,j]), 'Proposed MST contains an edge not found in the original graph' #test the edge found in the MST is approx_equal to the edge between the same nodes in the original adj_mat
     
     #3) Check MST has expected number of edges
     num_edges = 0
@@ -76,20 +68,16 @@ def check_mst(adj_mat: np.ndarray,
     for i in range(mst.shape[0]):
         for j in range(i+1): #iterate through elements in lower triangle of MST
             if mst[i,j] > 0: #if element is an edge (i.e, weight > 0)
-                num_edges +=1 #add another edge to the tally
-    assert num_edges == num_nodes - 1, "Proposed MST does not contain expected n-1 # of nodes"
+                num_edges +=1 #add another edge to the tally of total edges
+    assert num_edges == num_nodes - 1, "Proposed MST does not contain expected n-1 # of nodes" #test the MST has n-1 edges, where n is the number of nodes in the graph
 
     #4) Check if MST is connected 
-    mst_bfs = BFS(mst) #instantiate BFS class from project2 with mst matrix. the BFS class was adapted for this project to create an undirected graph (directed graphs were used in project 2)
+    mst_bfs = BFS(mst) #instantiate BFS class from project2 with mst matrix. the BFS class was adapted for this project to use an undirected graph (directed graphs were used in project 2)
     bfs_traversal_list = mst_bfs.bfs(start=0) #arbitrarily choose 0th node to start traversal from. Doesn't matter where I start bfs because graph is undirected so it will always traverse the whole thing
     assert len(bfs_traversal_list) == num_nodes #check that the number of nodes traversed via bfs is the same as the number of nodes in the graph, demonstrating connectivity
 
 
-    #Check that the MST edges actually form a path
 
-    #check there are no cycles
-
-    #given an undirected graph wth unique edge weights, check the MST is unique (by starting at different vertices and seeing what you get)
 
 
 def test_mst_small():
@@ -121,20 +109,19 @@ def test_find_unique_mst():
 
     If an undirected graph has unique weights for its edges (i.e, no two edges have the same weight) then
     there is one and only one MST for that graph. Proof by contradiction: Let there be two MSTs for a graph with unique edge weights, MST1 and MST2.
-    Since MST1 and MST2 are different despite containing the same nodes and despite being connected (by definition of an MST) they must differ by at least one edge
+    Since MST1 and MST2 are different despite containing the same nodes and despite being connected (by definition of an MST) they must differ by at least one edge.
     Assume they differ by an edge e that is the lowest weight edge possible. There is no other edge in the graph with the same weight because all edge weights are unique.
     If e were to be added to MST2, a cycle C would form because MST2 is an MST. MST1, by definition, does not have any cycles, so there must be an edge e2 in the cycle C that 
     is not in MST1, or else a cycle would exist in MST1. e2 must have higher weight than e1 because e1 was chosen to be the edge with minimum weight in the graph. 
-    e1 could replace e2 in MST2 and MST2 would still be an MST and connected. Therefore, if MST2 was replaced with e1, it would contradict the assertion that MST2 is an MST,
-    because it now has a lower total weight.
-
+    e1 could replace e2 in MST2 and MST2 would still be an MST (and connected). Therefore, if MST2 was replaced with e1, it would contradict the assertion that MST2 is an MST,
+    because it now has a lower total weight --> proof by contradiction that a graph with unique edge weights has a unique MST
 
     This test therefore tests whether a graph with unique edge weights `small_unique.csv` has a unique mst by constructing 
     the mst repeatedly and starting with a different starting node at each iteration. The resulting mst, no matter what node 
-    construction was started at, should be identical. This test asserts that is the case
+    construction was started at, should be identical. This test asserts that is the case and tests for proper MST construction by showing that, if I input a graph with unique edge weights, a unique MST will be constructed
 
     """
-    file_path = './data/small_unique.csv' #use small_unique.csv which is same as small.csv except one of the edges with weight 5 now has weight 3 and therefore all edge weights are unique
+    file_path = './data/small_unique.csv' #use small_unique.csv, which is the same as small.csv except one of the edges with weight 5 now has weight 3 and therefore all edge weights are unique
     g = Graph(file_path) #instantiate Graph with unique edge weights
     num_nodes = g.adj_mat.shape[0] #number of nodes in g
     for starting_node in range(num_nodes):
@@ -143,7 +130,8 @@ def test_find_unique_mst():
             baseline_mst = g.mst #the mst that comes from construction starting at node 0 will be compared to the mst constructed starting at each of the other nodes
         else:
             #this line tests that g.mst, which is the mst constructed starting with the starting_node at this iteration is similar within a very small tolerance to the mst constructed starting at node 0
-            assert np.allclose(g.mst, baseline_mst), 'There is a problem with your MST construction because the MST for this graph should be unique!'
+            #This tests for proper MST construction by showing that, if I input a graph with unique edge weights, a unique MST will be constructed
+            assert np.allclose(g.mst, baseline_mst), 'There is a problem with your MST construction because the MST for this graph should be unique!' 
 
 
 
