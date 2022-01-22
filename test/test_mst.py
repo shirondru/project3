053a95d149427423204c84b46454b129d741f38c
shirondru <59421145+shirondru@users.noreply.github.com>
2022-01-21 17:28:06 -0800

    Add test_find_unique_mst

--- test/test_mst.py
+++ test/test_mst.py
@@ -23,6 +23,14 @@ def check_mst(adj_mat: np.ndarray,
             Add additional assertions to ensure the correctness of your MST implementation
         For example, how many edges should a minimum spanning tree have? Are minimum spanning trees
         always connected? What else can you think of?
+
+
+
+        ###################################### Added assertions ######################################
+        1) Check proposed MST is symmetric: 
+        Because the input graph is undirected, the MST adjacency matrix should be symmetric. This also checks that the upper triangle of the MST adjacency matrix
+        has the expected edge weights, as the lower triangle was already validated by `check_mst` and this ensures the two triangles are identical.
+        This test checks for symmetry by checking that each element in the MST adjacency matrix and the same element in the transposed matrix are similar within a very small tolerance
     """
     def approx_equal(a, b):
         return abs(a - b) < allowed_error
@@ -32,6 +40,23 @@ def check_mst(adj_mat: np.ndarray,
         for j in range(i+1): 
             total += mst[i, j]
     assert approx_equal(total, expected_weight), 'Proposed MST has incorrect expected weight'
+
+
+    #Check proposed MST matrix is symmetric
+    assert np.allclose(g.mst, g.mst.T), 'Proposed MST adjacency matrix is not symmetric'
+
+
+
+    #Check that all of the edges in MST are also in the adj_mat
+
+    #Check that the MST edges actually form a path
+
+    #Check MST has correct number of edges
+    #Check if MST is connected
+
+    #check there are no cycles
+
+    #given an undirected graph wth unique edge weights, check the MST is unique (by starting at different vertices and seeing what you get)
 
 
 def test_mst_small():
@@ -56,20 +81,36 @@ def test_mst_single_cell_data():
     g.construct_mst()
     check_mst(g.adj_mat, g.mst, 57.263561605571695)
 
-def test_mst_symmetry():
-    """ 
-    Because the input graph is undirected, the MST adjacency matrix should be symmetric. This also checks that the upper triangle of the MST adjacency matrix
-    has the expected edge weights, as the lower triangle was already validated by `check_mst` and this ensures the two triangles are identical.
 
-    This test checks for symmetry by checking that each element in the MST adjacency matrix and the same element in the transposed matrix are similar within a very small tolerance
+#test_mst_student
+def test_find_unique_mst():
+    """ TODO: Write at least one unit test for MST construction 
+
+    If an undirected graph has unique weights for its edges (i.e, no two edges have the same weight) then
+    there is one and only one MST for that graph. Proof by contradiction: Let there be two MSTs for a graph with unique edge weights, MST1 and MST2.
+    Since MST1 and MST2 are different despite containing the same nodes and despite being connected (by definition of an MST) they must differ by at least one edge
+    Assume they differ by an edge e that is the lowest weight edge possible. There is no other edge in the graph with the same weight because all edge weights are unique.
+    If e were to be added to MST2, a cycle C would form because MST2 is an MST. MST1, by definition, does not have any cycles, so there must be an edge e2 in the cycle C that 
+    is not in MST1, or else a cycle would exist in MST1. e2 must have higher weight than e1 because e1 was chosen to be the edge with minimum weight in the graph. 
+    e1 could replace e2 in MST2 and MST2 would still be an MST and connected. Therefore, if MST2 was replaced with e1, it would contradict the assertion that MST2 is an MST,
+    because it now has a lower total weight.
+
+
+    This test therefore tests whether a graph with unique edge weights `small_unique.csv` has a unique mst by constructing 
+    the mst repeatedly and starting with a different starting node at each iteration. The resulting mst, no matter what node 
+    construction was started at, should be identical. This test asserts that is the case
+
     """
-    file_path = './data/small.csv'
-    g = Graph(file_path)
-    g.construct_mst()
+    file_path = './data/small_unique.csv' #use small_unique.csv which is same as small.csv except one of the edges with weight 5 now has weight 3 and therefore all edge weights are unique
+    g = Graph(file_path) #instantiate Graph with unique edge weights
+    num_nodes = g.adj_mat.shape[0] #number of nodes in g
+    for starting_node in range(num_nodes):
+        g.construct_mst(starting_node = starting_node) #try constructing the mst for this graph starting with each possible starting node in the graph
+        if starting_node == 0:
+            baseline_mst = g.mst #the mst that comes from construction starting at node 0 will be compared to the mst constructed starting at each of the other nodes
+        else:
+            #this line tests that g.mst, which is the mst constructed starting with the starting_node at this iteration is similar within a very small tolerance to the mst constructed starting at node 0
+            assert np.allclose(g.mst, baseline_mst), 'There is a problem with your MST construction because the MST for this graph should be unique!'
 
-    assert np.allclose(g.mst, g.mst.T), 'Your MST adjacency matrix is not symmetric'
 
 
-def test_mst_student():
-    """ TODO: Write at least one unit test for MST construction """
-    pass
